fb7155e5d47a350a6ac1e5abe8e83d4c36b4c4ae
polypmer <exorable.ludos@gmail.com>
2016-12-13 13:39:22 -0500

    Added a models file.

--- main.go
+++ main.go
@@ -4,32 +4,7 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"time"
 )
-
-type Member struct {
-	Id   int
-	Name string
-}
-
-type List struct {
-	Title       string
-	Cards       []*Card
-	Subscribers []*Member
-
-	// Unused
-	Archived bool
-}
-
-type Card struct {
-	Info    string
-	File    *os.File
-	Members []*Member
-	Due     time.Time
-	// if unused
-	Archived bool
-}
 
 // NOTE: Fuck methods?
 /* List Methods, Not currently Methods? */
