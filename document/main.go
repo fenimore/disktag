cb0027a160f8af1dda070802e8645a3eca7c9b14
polypmer <exorable.ludos@gmail.com>
2016-12-14 15:28:14 -0500

    Add pdf examples to gitignore

--- document/main.go
+++ document/main.go
@@ -9,7 +9,7 @@ var pages []pdf.Page
 var content []pdf.Content
 
 func main() {
-	reader, err := pdf.Open("trust.pdf")
+	reader, err := pdf.Open("observations.pdf")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -24,9 +24,10 @@ func main() {
 	for i, page := range pages {
 		content[i] = page.Content()
 	}
+	return
 
-	for _, val := range content[110].Text {
-		fmt.Println(val.S, val.W)
+	for _, val := range content[20].Text {
+		fmt.Print(val.S) //, val.W)
 	}
 	// fmt.Println("content")
 	// fmt.Println(len(content[1].Text))
